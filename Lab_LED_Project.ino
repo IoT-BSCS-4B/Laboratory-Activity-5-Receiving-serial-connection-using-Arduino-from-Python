dc238823936ff62f5c6c208ca0c9e06b09ae0b66
Matthew David L. Cartagena <matthewcartagena.mdcstack@gmail.com>
2025-12-04 22:25:40 +0800

    Refactor LED input handling by removing duplicates
    Removed duplicate case statements for lowercase inputs and related Serial print statements.

--- Lab_LED_Project.ino
+++ Lab_LED_Project.ino
@@ -13,47 +13,31 @@ void loop() {
     switch (input) {
       // Toggle Red
       case 'R':
-      case 'r':
         toggleLED(RED_PIN);
-        Serial.println("Red Toggled");
         break;
 
       // Toggle Green
       case 'G':
-      case 'g':
         toggleLED(GREEN_PIN);
-        Serial.println("Green Toggled");
         break;
 
       // Toggle Blue
       case 'B':
-      case 'b':
         toggleLED(BLUE_PIN);
-        Serial.println("Blue Toggled");
         break;
 
       // All ON
       case 'A':
-      case 'a':
         turnAllOn();
-        Serial.println("All ON");
         break;
 
       // All OFF
       case 'O':
-      case 'o':
         turnAllOff();
-        Serial.println("All OFF");
         break;
       
-      // Handle newline characters (ignore them)
-      case '\n':
-      case '\r':
-        break;
-
       // Error handling
       default:
-        Serial.println("Error: Invalid Input");
         break;
     }
   }
